b0e57f69092e4eb0c8e00d73ce8997d3365b5f9d
Аносов Александр Викторович <aanosov@cinimex.ru>
2025-03-11 23:03:55 +0300

    Deploy Workflow.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import federation from '@originjs/vite-plugin-federation';
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/vite-host-app-example/',
     plugins: [
         react(),
         federation({
